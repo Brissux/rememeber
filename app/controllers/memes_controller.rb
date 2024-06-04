cb369a767292ff647e89bdeb893bd4c4ba6b955d
Amin Sanouj <aminsanouj.dev@gmail.com>
2024-06-04 10:40:15 +0200

    New

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,4 +1,33 @@
 class MemesController < ApplicationController
-  before_action :authenticate_user!, except: %i[index show]
+  before_action :authenticate_user!, except: [:index, :show]
 
+  def index
+    @memes = Meme.where(public: true).order(created_at: :desc)
+  end
+
+  def new
+    @meme = Meme.new
+  end
+
+  def create
+    @meme = current_user.memes.build(meme_params)
+    if @meme.save
+      redirect_to "/index", notice: 'Mème créé avec succès !'
+      current_user.favorites.create(meme: @meme) if @meme.public
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @meme = Meme.find(params[:id])
+    @like = current_user.likes.find_by(meme: @meme)
+    @favorite = current_user.favorites.find_by(meme: @meme)
+  end
+
+  private
+
+  def meme_params
+    params.require(:meme).permit(:title, :public, :image, tag_names: [])
+  end
 end
--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,9 +1,20 @@
 class Meme < ApplicationRecord
   belongs_to :user
-
+  has_one_attached :image
   validates :title, presence: true
 
   has_many :likes
   has_many :favorites
+  has_many :meme_tags
   has_many :tags, through: :meme_tags
+
+  #after_save :save_tags
+
+  private
+
+  #def save_tags
+    #self.tags = self.tag_ids.split(',').map do |name|
+      #Tag.find_or_create_by(name: name.strip.downcase)
+    #end
+  #end
 end
