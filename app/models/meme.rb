97f647461c44ae44666f2e2bd494193615b8a1ac
Amin Sanouj <aminsanouj.dev@gmail.com>
2024-06-04 17:39:19 +0200

    Show tag ok

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -2,15 +2,15 @@ class Meme < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   validates :title, presence: true
+  validates :image, presence: true
 
   has_many :likes
   has_many :favorites
   has_many :meme_tags
   has_many :tags, through: :meme_tags
 
+  accepts_nested_attributes_for :meme_tags
   #after_save :save_tags
-
-  private
 
   #def save_tags
     #self.tags = self.tag_ids.split(',').map do |name|
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
   has_many :memes, through: :meme_tags
+  has_many :meme_tags
 
-  validates :name, presence: true, uniqueness: true
+  #validates :name, presence: true, uniqueness: true
 end
