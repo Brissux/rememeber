48c551bbda6c224af1badcc998759d66ebfc34e8
julienahlsweh <ahlsweh.julien@live.fr>
2024-06-06 11:00:45 +0200

    font

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@ amin = User.create!(email: "amin@gmail.com", nickname: "Amin", password: "passwo
 meme1 = Meme.create!(title: "Alien Theory explication", public: true, user: antoine)
 meme1.image.attach(io: URI.open("https://i.imgflip.com/26am.jpg"), filename: 'alien_theory.jpg')
 
-meme2 = Meme.create!(title: "Bouton stress", public: true, user: julien)
-meme2.image.attach(io: URI.open("https://i.imgflip.com/1g8my4.jpg"), filename: 'bouton_stress.jpg')
+meme2 = Meme.create!(title: "Hide the pain", public: true, user: julien)
+meme2.image.attach(io: URI.open("https://content.imageresizer.com/images/memes/Hide-the-pain-Harold-large-meme-8.jpg"), filename: 'hide_the_pain.jpg')
 
 meme3 = Meme.create!(title: "Cerveau Explosion", public: false, user: anais)
 meme3.image.attach(io: URI.open("https://i.imgflip.com/1jwhww.jpg"), filename: 'cerveau_explos.jpg')
@@ -25,9 +25,42 @@ meme3.image.attach(io: URI.open("https://i.imgflip.com/1jwhww.jpg"), filename: '
 meme4 = Meme.create!(title: "Drake Dance", public: true, user: amin)
 meme4.image.attach(io: URI.open("https://i.imgflip.com/30b1gx.jpg"), filename: 'drake_dance.jpg')
 
+meme5 = Meme.create!(title: "Drake Dance", public: true, user: amin)
+meme5.image.attach(io: URI.open("https://i.imgflip.com/30b1gx.jpg"), filename: 'drake_dance.jpg')
+
+meme6 = Meme.create!(title: "Spiderman Glasses", public: true, user: antoine)
+meme6.image.attach(io: URI.open("https://i.kym-cdn.com/photos/images/newsfeed/002/102/993/5c6.png"), filename: 'spiderman_glasses.png')
+
+meme7 = Meme.create!(title: "Disaster Girl", public: true, user: anais)
+meme7.image.attach(io: URI.open("https://images-prod.dazeddigital.com/900/azure/dazed-prod/1300/6/1306998.jpeg"), filename: 'disaster_girl.jpeg')
+
+meme8 = Meme.create!(title: "Foucault Meteo", public: true, user: amin)
+meme8.image.attach(io: URI.open("https://images3.memedroid.com/images/UPLOADED26/665ed3e73a291.webp"), filename: 'foucault_meteo.webp')
+
+meme9 = Meme.create!(title: "Chance Vie", public: true, user: julien)
+meme9.image.attach(io: URI.open("https://images3.memedroid.com/images/UPLOADED782/666035d9645cd.webp"), filename: 'chance_vie.webp')
+
+meme10 = Meme.create!(title: "Architecte Escalier", public: true, user: julien)
+meme10.image.attach(io: URI.open("https://images7.memedroid.com/images/UPLOADED765/665ed3da34938.webp"), filename: 'architecte_escalier.webp')
+
+meme11 = Meme.create!(title: "Playlist Pizza", public: true, user: anais)
+meme11.image.attach(io: URI.open("https://images7.memedroid.com/images/UPLOADED621/6656ecba89bd8.webp"), filename: 'playlist_pizza.webp')
+
+meme12 = Meme.create!(title: "Tennis France", public: true, user: antoine)
+meme12.image.attach(io: URI.open("https://images3.memedroid.com/images/UPLOADED460/6655c4386364f.webp"), filename: 'tennis_france.webp')
+
+meme13 = Meme.create!(title: "Voiture Copine", public: true, user: julien)
+meme13.image.attach(io: URI.open("https://images3.memedroid.com/images/UPLOADED954/66545147bc814.webp"), filename: 'voiture_copine.webp')
+
+meme14 = Meme.create!(title: "Mecanicien Chut", public: true, user: antoine)
+meme14.image.attach(io: URI.open("https://images3.memedroid.com/images/UPLOADED198/6651f34478a2b.webp"), filename: 'mecanicien_chut.webp')
+
+meme15 = Meme.create!(title: "Metro After", public: true, user: anais)
+meme15.image.attach(io: URI.open("https://images7.memedroid.com/images/UPLOADED319/665033ef87a68.webp"), filename: 'metro_after.webp')
+
 # Create tags
 tag_alien_theory = Tag.create!(name: "alien theory")
-tag_bouton = Tag.create!(name: "bouton")
+tag_hide_the_pain = Tag.create!(name: "pain")
 tag_cerveau = Tag.create!(name: "cerveau")
 tag_drake = Tag.create!(name: "drake")
 tag_funny = Tag.create!(name: "funny") # Common tag
@@ -35,13 +68,13 @@ tag_reaction = Tag.create!(name: "reaction") # Common tag
 
 # Create meme_tags
 MemeTag.create!(meme: meme1, tag: tag_alien_theory) # Alien Theory explication - alien theory
-MemeTag.create!(meme: meme2, tag: tag_bouton) # Bouton stress - bouton
+MemeTag.create!(meme: meme2, tag: tag_hide_the_pain) # Hide the pain - pain
 MemeTag.create!(meme: meme3, tag: tag_cerveau) # Cerveau Explosion - cerveau
 MemeTag.create!(meme: meme4, tag: tag_drake) # Drake Dance - drake
 
 # Associate common tags with memes
 MemeTag.create!(meme: meme1, tag: tag_funny) # Alien Theory explication - funny (common tag)
-MemeTag.create!(meme: meme2, tag: tag_reaction) # Bouton stress - reaction (common tag)
+MemeTag.create!(meme: meme2, tag: tag_hide_the_pain) # Bouton stress - reaction (common tag)
 MemeTag.create!(meme: meme3, tag: tag_funny) # Cerveau Explosion - funny (common tag)
 MemeTag.create!(meme: meme4, tag: tag_reaction) # Drake Dance - reaction (common tag)
 
