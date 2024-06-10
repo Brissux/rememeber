608e9b4819aa977c121be5237786d90dbd50aece
Amin Sanouj <aminsanouj.dev@gmail.com>
2024-06-10 12:08:19 +0200

    Tag button input ok

---
+++ app/javascript/controllers/tag_input_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["input", "tags"]
+
+  connect() {
+    this.inputTarget.addEventListener("keydown", this.handleKeyDown.bind(this))
+  }
+
+  handleKeyDown(event) {
+    if (event.key === ",") {
+      event.preventDefault()
+      this.addTag(event.target.value.trim())
+      event.target.value = ""
+    }
+  }
+
+  addTag(name) {
+    const tag = document.createElement("div")
+    tag.classList.add("button-tag-input", "d-flex", "align-items-center", "gap-1")
+    tag.innerHTML = `
+      <div class="d-flex align-items-center justify-content-center">
+          <p class="m-0" style="width: auto; padding-right: 5px;">${name}</p>
+          <input type="hidden" name="meme[meme_tags_attributes][][tag_attributes][name]" value="${name}">
+          <button type="button" class="btn btn-close btn-sm" style="font-size: 0.8rem; margin-left: 2px;" data-action="click->tag-input#removeTag"></button>
+      </div>
+
+    `
+    this.tagsTarget.appendChild(tag)
+  }
+
+  removeTag(event) {
+    const tag = event.target.closest(".button-tag")
+    tag.remove()
+  }
+}
