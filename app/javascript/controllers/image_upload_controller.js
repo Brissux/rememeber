cf3c9ac4cdc021b366279a4e5125422c0abdb080
Antoine Brisson <brissux.antoine@gmail.com>
2024-06-11 09:25:32 +0200

    ok

--- app/javascript/controllers/image_upload_controller.js
+++ app/javascript/controllers/image_upload_controller.js
@@ -1,23 +1,32 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="image-upload"
-
 export default class extends Controller {
   static targets = [ "output", "input" ]
-
 
   readURL() {
     var input = this.inputTarget
     var output = this.outputTarget
 
     if (input.files && input.files[0]) {
+      var file = input.files[0];
       var reader = new FileReader();
 
       reader.onload = function () {
-       output.src = reader.result
-     }
+        if (file.type.startsWith("video/")) {
+          output.style.display = "block";
+          output.src = reader.result;
+          output.play();
+        } else if (file.type.startsWith("image/")) {
+          output.style.display = "block";
+          output.src = reader.result;
+        }
+      }
 
-     reader.readAsDataURL(input.files[0]);
-   }
+      reader.readAsDataURL(file);
+    }
+  }
+
+  addDnone() {
   }
 }
