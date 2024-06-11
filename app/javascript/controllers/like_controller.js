9ef8a677e68eb56d0b219759412d500330897720
Amin Sanouj <aminsanouj.dev@gmail.com>
2024-06-11 11:40:49 +0200

    a valider Louis

--- app/javascript/controllers/like_controller.js
+++ app/javascript/controllers/like_controller.js
@@ -1,10 +1,39 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["form"]
+  static targets = ["empty", "full", "likeForm", "unlikeForm"]
+  static values = {
+    id: Number
+  }
 
   like(event) {
     event.preventDefault()
-    this.formTarget.submit()
+    const url = this.likeFormTarget.action
+    console.log(this.idValue)
+
+    fetch(url, {
+      method: "POST",
+      headers: { "Accept": "application/json" },
+        // body: new FormData(this.likeFormTarget)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+  }
+
+  unlike(event) {
+    event.preventDefault()
+    const url = this.unlikeFormTarget.action
+
+    // fetch(url, {
+    //   method: "DELETE",
+    //   headers: { "Accept": "application/json" },
+    //   // body: new FormData(this.unlikeFormTarget)
+    // })
+    //   .then(response => response.json())
+    //   .then((data) => {
+    //     console.log(data)
+    //   })
   }
 }
