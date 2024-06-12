85662e05d9e14e53ae45e88737ce2170ae75a620
Amin Sanouj <aminsanouj.dev@gmail.com>
2024-06-12 16:16:43 +0200

    navbar favoris ok

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,15 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if params[:search].present?
+    if params[:filter] == "my_memes"
+      @favorites = current_user.favorites.joins(:meme).where(memes: { user_id: current_user.id })
+    elsif params[:search].present?
       @favorites = current_user.favorites.joins(:meme).merge(Meme.search_by_title_and_tag(params[:search]))
     else
       @favorites = current_user.favorites
     end
   end
-  
+
   def create
     @meme = Meme.find(params[:meme_id])
     @favorite = Favorite.new
