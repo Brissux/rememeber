ed8d0d12045cd29b7af08eb12abcf1a17bb4e75c
Anais Khader <anais.khader@gmail.com>
2024-06-14 11:41:21 +0200

    .

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["form"]
+
+  connect() {
+    console.log(this.formTarget)
+  }
+
+  submit(event) {
+    console.log(this.formTarget)
+    this.formTarget.submit()
+  }
+}
